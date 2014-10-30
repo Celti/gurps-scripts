d3e10d426f8548f044835ba8b8954095a4eea500
Patrick Burroughs (Celti) <celticmadman@gmail.com>
2014-10-29 23:08:32 -0700

    Added dokuwiki version of the point calculator.

---
+++ gurps-point-calculator-web.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+# GURPS Point Calculator (Web Edition)
+# Intended to read character sheets from dokuwiki pages and provide output to be displayed via AJAX.
+# (c) 2007-2014 Patrick Burroughs (Celti) <celticmadman@gmail.com>
+
+## Testing:
+# Wealth (Filthy Rich) [50]
+# Greed (12) [-15]
+# Template: Immortal [5]
+#    Unaging <5>
+# 
+# $2, $400, $60K, $800M
+# 10.5 lbs., 8 oz.
+# {10} |2| {20} |11|
+
+use common::sense;
+
+my $data_dir = '/home/public/wiki/data/pages/';
+my $page_name = $ENV{'QUERY_STRING'};
+
+my (@points, @disads);
+my (@angle, @curly, @pipe);
+my (@money, @weight);
+
+sub sum {
+	my $s = 0;
+	($s+=$_) for @_;
+	$s =~ s/(?<=\d)(?=(?:\d\d\d)+(?!\d))/,/g;
+	return $s;
+}
+
+die "Attempt to access files outside of data directory!" if $page_name =~ /\.\.\//;
+open my $sheet, "<", $data_dir.$page_name or die "Couldn't open character sheet.";
+
+while (<$sheet>) {
+	s/½|1\/2/.5/g;
+	s/\+//g;
+
+	foreach (/\[(-?\d*\.?\d+)\]/g) { push @points, $_; }
+	foreach (/\[(-\d*\.?\d+)\]/g)  { push @disads, $_; }
+
+	foreach (/\<(-?\d*\.?\d+)\>/g) { push @angle, $_; }
+	foreach (/\{(-?\d*\.?\d+)\}/g) { push @curly, $_; }
+	foreach (/\|(-?\d*\.?\d+)\|/g) { push @pipe, $_; }
+	
+	foreach (/\$(-?(?:\d{1,3},)*\d*\.?\d+(?:K|M|B|T)?)/g) {
+		s/,//g;
+		$_ *= 1000 if s/K//;
+		$_ *= 1000000 if s/M//;
+		$_ *= 1000000000 if s/B//;
+		$_ *= 1000000000000 if s/T//;
+		push @money, $_;
+	}
+
+	foreach (/((?:\d{1,3},)*\d*\.?\d+)\s*lbs?\.?/xig)
+		{ s/,//g; push @weight, $_; }
+	foreach (/((?:\d{1,3},)*\d*\.?\d+)\s*oz\./xig)
+		{ s/,//g; push @weight, $_/16; }
+}
+
+print "Content-type: text/html\n\n";
+printf "%s points (%s disadvantages)<br/>\n", sum(@points), sum(@disads);
+printf "Equipment: \$%s, %s lbs.<br/>\n", sum(@money), sum(@weight);
+printf "Other sums: &lt;%s&gt; {%s} |%s|<br/>\n", sum(@angle), sum(@curly), sum(@pipe);
